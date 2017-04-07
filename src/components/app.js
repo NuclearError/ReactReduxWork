49689dba1ae06a3f27c22e259fc56b5bcedd0069
Angela Cullen <angela.cullen@immediate.co.uk>
2017-04-07 13:57:43 +0100

    Reset files to be more like example kit from Stephen Grider's github

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,9 @@
-import React, { component } from 'react';
-
-import BookList from '../containers/book-list';
+import React, { Component } from 'react';
 
 export default class App extends Component {
-
   render() {
     return (
-      <div className='book-list__container'>
-        <BookList />
-      </div>
+      <div>React simple starter</div>
     );
-  };
-
+  }
 }
