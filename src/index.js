ee94e5abf80fa0d9ba71a48c5c72f1b340ee18e6
Angela Cullen <angela.cullen@immediate.co.uk>
2017-04-07 12:19:38 +0100

    Reset working files for next app build in the tutorial

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import _ from 'lodash'; // npm install --save lodash
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-
-// Youtube Public API
-const API_KEY = 'AIzaSyAvNjPln8Hb3Z3qhYa3H9DTJt0Mq4oHu6M';
-
-/*
-// Create a new component; this component should produce some HTML.
-// This is a CLASS, not an object instance.
-// This way of writing it is a functional component (as opposed to class-based)
-const App = () => {
-	return (
-		<section>
-			<SearchBar />
-			<VideoList />
-		</section>
-	);
-};
-*/
 
 class App extends Component {
 
@@ -30,42 +8,14 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			videos: [],
-			selectedVideo: null
+			//
 		};
-
-		this.performVideoSearch('leggings sewing');
 	};
 
-	// Youtube API search is now contained in its own method
-	performVideoSearch(term) {
-		YTSearch( {key: API_KEY, term: term}, (videos) => {
-			this.setState({
-				videos: videos,
-				selectedVideo: videos[0]
-			});
-		});
-	};
-
-	// SearchBar has the property 'onSearchTermChange' which we can call back
-	// to when the input change event fires
 	render() {
 
-		// inner function wrapped inside a debounce method (using lodash)
-		// - this function won't call until 300ms have elapsed, no matter how many times it is called
-		const throttledVideoSearch = _.debounce( (term) => { this.performVideoSearch(term), 300 });
-
 		return (
-			// 'onSearchTermChange' can be called as often as possible, but will
-			// still only run every 300ms
-			<section>
-				<SearchBar onSearchTermChange={throttledVideoSearch} />
-				<VideoDetail video={this.state.selectedVideo} />
-				<VideoList
-					onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
-					videos={this.state.videos}
-				/>
-			</section>
+			//
 		);
 	};
 }
