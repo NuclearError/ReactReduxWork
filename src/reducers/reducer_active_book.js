51b198cd51d1107af390f19e8514ebbdb3a1c2f9
Angela Cullen <angela.cullen@immediate.co.uk>
2017-04-07 15:38:09 +0100

    Another bug, this time ./reducer_active_book can't be found

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,16 +1,23 @@
+export default function(state = null, action) {
+
+  switch(action.type) {
+  case: 'BOOK_SELECTED':
+    return action.payload;
+  }
+
+  return state;
+}
+
+
 // The state argument is not the application state,
 // only the state that this reducer is responsible for.
 
 // State is initially set to null because reducers are not
 // allowed to return 'undefined' (eg. when app starts)
-export default function(state = null, action) {
-  // state += 1 // basic example of incrementation caused by an action
 
-  switch(action.type) {
-  case: 'BOOK_SELECTED':
-    // avoid doing things like "state.title = book.title"
-    // instead, always return a fresh object as below
-    return action.payload;
-  }
-  return state;
-}
+// state += 1 // basic example of incrementation caused by an action
+
+// avoid doing things like "state.title = book.title"
+// instead, always return a fresh object as below
+
+  // in default cases, the state is returned unchanged
