bb67e87a06f0208dc699ce668a1fe00d4323503a
Angela Cullen <angela.cullen@immediate.co.uk>
2017-06-02 17:56:59 +0100

    Prop undefined error on this.props.weather

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+    // create new array containing old stuff + new payload
+    // return [ new stuff, ...old stuff] //
+    // (adds the old stuff to the new stuff)
+    return [ action.payload.data, ...state];
+    // Alternative way to write this:
+    // return state.concat([action.payload.data]);
+  }
   return state;
 }
