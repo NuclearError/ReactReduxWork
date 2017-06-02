ba4f793b3c28b8bdaa9b68ed6b5ea9ec6328e30b
Angela Cullen <angela.cullen@immediate.co.uk>
2017-06-02 14:18:44 +0100

    Initial setup of WEATHER app

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <p>Hello World</p>
+      <div className="wrapper">
+        <h3>Weather App</h3>
+        <SearchBar />
+      </div>
     );
   }
 }
